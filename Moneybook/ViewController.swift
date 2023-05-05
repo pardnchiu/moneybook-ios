9fb898f2cc42d7d6875002ba09210b7a6c2c1269
Macbook Pro 14 <develper@pardn.io>
2023-05-05 21:31:29 +0800

    Initial Commit

---
+++ Moneybook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moneybook
+//
+//  Created by Pardn on 2023/5/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
